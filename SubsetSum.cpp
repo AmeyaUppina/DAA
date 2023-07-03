fcab8dc3c7252fa27081a18eee44048a61ecb63a
Ameya Uppina <ameyauppina2003@gmail.com>
2023-07-03 18:34:29 +0530

    Update SubsetSum.cpp

--- SubsetSum.cpp
+++ SubsetSum.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
 
-int count = 0;
+//int count = 0;
 
-void subsetsum(int list[], int sum, int start, int target, int size, int subset[], int subsetSize) 
+int subsetsum(int list[], int sum, int start, int target, int size, int subset[], int subsetSize, int count)
 {
-    if (sum == target) 
+    if (sum == target)
     {
         count++;
         for (int i = 0; i < subsetSize; i++) 
@@ -15,27 +15,31 @@ void subsetsum(int list[], int sum, int start, int target, int size, int subset[
         cout << endl;
     }
 
-    if (start >= size) 
+    if (start >= size)
     {
-        return;
+        return count;
     }
 
-    for (int i = start; i < size; i++) 
+    for (int i = start; i < size; i++)
     {
-        if (sum + list[i] <= target) {
+        if (sum + list[i] <= target)
+        {
             subset[subsetSize] = list[i];
-            subsetsum(list, sum + list[i], i + 1, target, size, subset, subsetSize + 1);
+            count = subsetsum(list, sum + list[i], i + 1, target, size, subset, subsetSize + 1, count);
         }
     }
+
+    return count;
 }
 
-int main() 
+int main()
 {
     int nums[] = {5, 10, 12, 13, 15, 18};
     int target = 30;
     int size = sizeof(nums) / sizeof(nums[0]);
     int subset[size];
-    subsetsum(nums, 0, 0, target, size, subset, 0);
-    cout<<"Total possible subsets: "<<count<<"\n";
+    int count = 0;
+    int ans =  subsetsum(nums, 0, 0, target, size, subset, 0, count);
+    cout<<"Total possible subsets: "<<ans<<"\n";
     return 0;
 }
