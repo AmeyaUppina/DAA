3d17da6dc7eca0e89e26819c618568ea85b3de24
Ameya Uppina <ameyauppina2003@gmail.com>
2025-05-08 11:30:22 +0530

    Update BellmanFord.cpp

--- BellmanFord.cpp
+++ BellmanFord.cpp
@@ -1,44 +1,57 @@
 #include <iostream>
+#include <vector>
+#include <limits>
+
 using namespace std;
 
-#define INF 999999
+const int INF = numeric_limits<int>::max();
 
 int main() {
-    int v = 7;    //total no. of nodes
-    int cost[7][7] = {
-            {0, 6, 5, 5, 0, 0, 0},
-            {0, 0, 0, 0, -1, 0, 0},
-            {0, -2, 0, 0, 1, 0, 0},
-            {0, 0, -2, 0, 0, -1, 0},
-            {0, 0, 0, 0, 0, 0, 3},
-            {0, 0, 0, 0, 0, 0, 3},
-            {0, 0, 0, 0, 0, 0, 0}
-    };             //cost matrix initialization.
+    int num_vertices = 5;
 
-    int d[7] = {0, INF, INF, INF, INF, INF, INF}; //Source node distance is marked as 0, in this case node 0.
+    // Cost matrix representing the directed graph
+    vector<vector<int>> cost_matrix = {
+        {0, -1, INF, INF, INF},
+        {INF, 0, 3, 2, 2},
+        {INF, INF, 0, INF, INF},
+        {INF, 1, 5, 0, INF},
+        {INF, INF, INF, -3, 0}
+    };
 
-    for(int k=0; k<v; k++)
-    {
-        for(int i=0; i<v; i++)
-        {
-            for(int j=0; j<v; j++)
-            {
-                if(cost[i][j] != 0)
-                {
-                    if(d[i]+cost[i][j]<d[j])
-                    {
-                        d[j] = d[i]+cost[i][j];
-                    }
+    int source_node = 0;
+    vector<int> distance(num_vertices, INF);
+    distance[source_node] = 0;
+
+    // Relax all edges |V| - 1 times
+    for (int i = 1; i <= num_vertices - 1; ++i) {
+        for (int u = 0; u < num_vertices; ++u) {
+            for (int v = 0; v < num_vertices; ++v) {
+                if (cost_matrix[u][v] != INF && distance[u] != INF && distance[u] + cost_matrix[u][v] < distance[v]) {
+                    distance[v] = distance[u] + cost_matrix[u][v];
                 }
             }
         }
     }
 
-    for(int i=0; i<v; i++)
-    {
-        cout<<d[i]<<" ";
+    // Check for negative cycles, if after v-1 iterations, the cost can still be reduced, there is a negative cycle.
+    for (int u = 0; u < num_vertices; ++u) {
+        for (int v = 0; v < num_vertices; ++v) {
+            if (cost_matrix[u][v] != INF && distance[u] != INF && distance[u] + cost_matrix[u][v] < distance[v]) {
+                cout << "Graph contains a negative cycle" << endl;
+                return 1;
+            }
+        }
+    }
+
+    cout << "Shortest distances from source node " << source_node << ":" << endl;
+    for (int i = 0; i < num_vertices; ++i) {
+        cout << "To node " << i << ": ";
+        if (distance[i] == INF) {
+            cout << "Infinity" << endl;
+        } else {
+            cout << distance[i] << endl;
+        }
     }
 
     return 0;
 }
-
